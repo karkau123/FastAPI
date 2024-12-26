cef02199156c3a77f8751882bc79a95a7fd92c9b
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2024-12-27 03:23:41 +0530

    fast api query params

--- main.py
+++ main.py
@@ -6,13 +6,42 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+# ? is the query parameter
+@app.get('/blog')   # this decorator will run the below function at some port no
+def index(limit=10, published:bool=True):
+    
+    if (published):
+        return {'data': f'{limit} published blogs from the Database'}
+    else:
+        return {'data': f'{limit} blogs from the Database'}
+    
 
-@app.get('/')
-def index():
-    return {'data':{'name' : 'Roshni'}}
 
 
-@app.get('/about')
-def about():
-    return{'data':{'about page'}}
+@app.get('blog/unpublished')
+def unpublished():
+    return {'data': "all unpublihed blogs"}
 
+
+@app.get('/blog/{id}')  # use curly braces as you want dynamic id 
+def show(id:int):
+    #fetch blog with id = id
+    return{'data' : id }
+
+
+
+
+
+ 
+
+@app.get('/blog/{id}/comments')  # use curly braces as you want dynamic id 
+def comments(id):
+    #fetch blog with id = id
+     return {
+            "data": [
+                {"id": 1, "comment": "First comment"},
+                {"id": 2, "comment": "Second comment"}
+            ]
+        }
+
+# for testing 
